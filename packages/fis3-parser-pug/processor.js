a055d0b735758d39fd78f59383185d221025c81d
fisker <lionkay@gmail.com>
2017-11-17 19:22:34 +0800

    remove useless log

--- packages/fis3-parser-pug/processor.js
+++ packages/fis3-parser-pug/processor.js
@@ -1,7 +1,6 @@
 'use strict'
 
 var render = require('pug').render
-var log = global.fis.log
 
 module.exports = function(content, file, conf) {
   return content ? render(content, conf) : ''
--- src/packages/parser/pug/processor.js
+++ src/packages/parser/pug/processor.js
@@ -1,5 +1,4 @@
 const render = require('pug').render
-const log = global.fis.log
 
 module.exports = function(content, file, conf) {
   return content ? render(content, conf) : ''
