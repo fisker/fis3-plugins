50e6267a64b7caf79c034efd68de10637f6b0d73
fisker <lionkay@gmail.com>
2018-01-02 11:15:58 +0800

    update

--- src/packages/server/browsersync/info.js
+++ src/packages/server/browsersync/info.js
@@ -1,5 +1,5 @@
 module.exports = {
-  version: '1.4.3',
+  version: '1.4.4',
   description: 'a browser sync server for fis3.',
   keywords: ['browser-sync', 'livereload'],
   dependencies: ['browser-sync', 'filesize', 'lodash.merge', 'serve-directory'],
