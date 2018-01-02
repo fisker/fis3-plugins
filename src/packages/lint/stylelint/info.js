cc98d79ff171880f3df9d0eb0188f1a5b22dfcf1
fisker <lionkay@gmail.com>
2018-01-02 11:24:27 +0800

    update postcss

--- src/packages/lint/stylelint/info.js
+++ src/packages/lint/stylelint/info.js
@@ -1,5 +1,5 @@
 module.exports = {
-  version: '2.0.6',
+  version: '2.0.7',
   description: 'a css linter plugin of fis3 based on stylelint.',
   keywords: ['linter'],
   dependencies: [
--- src/packages/postprocessor/stylefmt/info.js
+++ src/packages/postprocessor/stylefmt/info.js
@@ -1,5 +1,5 @@
 module.exports = {
-  version: '1.0.6',
+  version: '1.0.7',
   description: 'a css formatter of fis3 based on stylefmt.',
   keywords: [],
   dependencies: ['es6-shim', 'postcss', 'promise-synchronizer', 'stylefmt'],
