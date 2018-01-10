046f18eec890abc5546cdb3b3afd2935f62e6887
fisker <lionkay@gmail.com>
2018-01-10 18:02:51 +0800

    code style

--- src/packages/server/browsersync/info.js
+++ src/packages/server/browsersync/info.js
@@ -2,14 +2,15 @@ module.exports = {
   version: '1.5.5',
   description: 'a browser sync server for fis3.',
   keywords: ['browser-sync', 'livereload'],
-  dependencies: ['browser-sync', 'lodash.merge', 'serve-directory', 'serve-directory-theme-octicons'],
+  dependencies: [
+    'browser-sync',
+    'lodash.merge',
+    'serve-directory',
+    'serve-directory-theme-octicons'
+  ],
   options: {},
   links: {
     browsersync: 'https://browsersync.io/'
   },
-  files: [
-    'app.js',
-    'lib/browser-sync-config.js',
-    'lib/serve-directory.js'
-  ]
+  files: ['app.js', 'lib/browser-sync-config.js', 'lib/serve-directory.js']
 }
