01b1458714ac4560274325d8f78ed342ec230402
fisker <lionkay@gmail.com>
2018-01-15 11:21:37 +0800

    update dependencies

--- src/packages/postprocessor/autoprefixer-latest/info.js
+++ src/packages/postprocessor/autoprefixer-latest/info.js
@@ -1,5 +1,5 @@
 module.exports = {
-  version: '1.0.4',
+  version: '1.0.5',
   description: 'latest version autoprefixer for fis3.',
   keywords: ['autoprefixer'],
   dependencies: ['postcss', 'autoprefixer'],
--- src/packages/postprocessor/prettier/info.js
+++ src/packages/postprocessor/prettier/info.js
@@ -1,5 +1,5 @@
 module.exports = {
-  version: '1.0.8',
+  version: '1.0.9',
   description: 'a code formatter of fis3 based on prettier.',
   keywords: ['beautify', 'format', 'formatter'],
   dependencies: ['prettier'],
