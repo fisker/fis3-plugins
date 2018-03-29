0812e72c2a30c40410cb9174bab646db521eef36
fisker <lionkay@gmail.com>
2018-03-29 16:20:22 +0800

    update dependencies

--- src/packages/postprocessor/posthtml-beautify/info.js
+++ src/packages/postprocessor/posthtml-beautify/info.js
@@ -1,5 +1,5 @@
 module.exports = {
-  version: '1.0.13',
+  version: '1.1.0',
   description: 'a html formatter of fis3 based on posthtml-beautify.',
   keywords: ['posthtml', 'beautify', 'html', 'format', 'formatter'],
   dependencies: ['posthtml', 'posthtml-beautify', 'promise-synchronizer'],
--- src/packages/postprocessor/stylefmt/info.js
+++ src/packages/postprocessor/stylefmt/info.js
@@ -1,5 +1,5 @@
 module.exports = {
-  version: '1.0.13',
+  version: '1.1.0',
   description: 'a css formatter of fis3 based on stylefmt.',
   keywords: [],
   dependencies: ['es6-shim', 'postcss', 'promise-synchronizer', 'stylefmt'],
