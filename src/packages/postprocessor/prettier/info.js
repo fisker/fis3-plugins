46a14778cbb276c4e82316855a9963a0421d2b46
fisker <lionkay@gmail.com>
2018-07-02 09:41:00 +0800

    update dependencies

--- src/packages/postprocessor/prettier/info.js
+++ src/packages/postprocessor/prettier/info.js
@@ -1,5 +1,5 @@
 module.exports = {
-  version: '1.3.6',
+  version: '1.3.7',
   description: 'a code formatter of fis3 based on prettier.',
   keywords: ['beautify', 'format', 'formatter'],
   dependencies: ['prettier'],
