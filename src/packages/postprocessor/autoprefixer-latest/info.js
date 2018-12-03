4f97884ae62cf09c23be3e0de3776572acc4f89d
fisker <lionkay@gmail.com>
2018-12-03 17:17:01 +0800

    update autoprefixer

--- src/packages/postprocessor/autoprefixer-latest/info.js
+++ src/packages/postprocessor/autoprefixer-latest/info.js
@@ -1,5 +1,5 @@
 module.exports = {
-  version: '1.1.19',
+  version: '1.1.20',
   description: 'latest version autoprefixer for fis3.',
   keywords: ['autoprefixer'],
   dependencies: ['postcss', 'autoprefixer'],
