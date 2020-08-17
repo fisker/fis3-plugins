4f516bd3526b4fe106ded71ed9bb1f43ef55d98a
fisker <lionkay@gmail.com>
2020-08-17 17:10:39 +0800

    refactor: use `promise-synchronizer` v3

--- src/packages/lint/stylelint/index.js
+++ src/packages/lint/stylelint/index.js
@@ -16,6 +16,48 @@ const syntax = {
   '.sss': 'sugarss',
 }
 
+const runStylelint = sync(async (content, config, file) => {
+  const result = await postcss([stylelint]).process(content, config)
+
+  const messages = result.messages || []
+  const errorMessage = []
+  const warnMessage = []
+  for (const message of messages) {
+    const type = message.severity || 'warn'
+    ;(type === 'error' ? errorMessage : warnMessage).push(
+      [
+        ' ',
+        `${type}:`,
+        message.line ? `[${message.line}:${message.column}]` : '',
+        // '[' + message.rule + ']',
+        message.text,
+      ].join(' ')
+    )
+  }
+
+  if (warnMessage.length > 0 || errorMessage.length > 0) {
+    log.warn(
+      '[%s] lint failed: \n%s \n  %s problem (%s errors, %s warning)',
+      file.id,
+      warnMessage.concat(errorMessage).join('\n'),
+      warnMessage.length + errorMessage.length,
+      errorMessage.length,
+      warnMessage.length
+    )
+
+    if (errorMessage.length > 0) {
+      process.exitCode = 1
+      throw new Error('stylelint error.')
+    }
+  }
+
+  if (result && result.css) {
+    return result.css
+  }
+
+  return ''
+})
+
 function process(content, file, config_) {
   if (!content) {
     return content
@@ -37,50 +79,8 @@ function process(content, file, config_) {
     config.syntax = syntax[file.ext]
   }
 
-  const promise = postcss([stylelint])
-    .process(content, config)
-    .then(function (result) {
-      const messages = result.messages || []
-      const errorMessage = []
-      const warnMessage = []
-      for (const message of messages) {
-        const type = message.severity || 'warn'
-        ;(type === 'error' ? errorMessage : warnMessage).push(
-          [
-            ' ',
-            `${type}:`,
-            message.line ? `[${message.line}:${message.column}]` : '',
-            // '[' + message.rule + ']',
-            message.text,
-          ].join(' ')
-        )
-      }
-
-      if (warnMessage.length > 0 || errorMessage.length > 0) {
-        log.warn(
-          '[%s] lint failed: \n%s \n  %s problem (%s errors, %s warning)',
-          file.id,
-          warnMessage.concat(errorMessage).join('\n'),
-          warnMessage.length + errorMessage.length,
-          errorMessage.length,
-          warnMessage.length
-        )
-
-        if (errorMessage.length > 0) {
-          process.exitCode = 1
-          throw new Error('stylelint error.')
-        }
-      }
-
-      if (result && result.css) {
-        return result.css
-      }
-
-      return ''
-    })
-
   try {
-    return sync(promise)
+    return runStylelint(content, config, file)
   } catch (error) {
     throw new Error(`[${file.id}] lint failed with error: \n\n ${error}`)
   }
--- src/packages/postprocessor/postcss-with-rc/index.js
+++ src/packages/postprocessor/postcss-with-rc/index.js
@@ -4,7 +4,11 @@ import sync from 'promise-synchronizer'
 import exportPlugin from '../../../shared/export-plugin'
 import * as info from './info'
 
-async function postcssProcess(content, file, config) {
+const postcssProcess = sync(async function postcssProcess(
+  content,
+  file,
+  config
+) {
   const {plugins, options} = await postcssrc()
   const {css} = await postcss(plugins).process(content, {
     ...config,
@@ -13,10 +17,10 @@ async function postcssProcess(content, file, config) {
   })
 
   return css
-}
+})
 
 function process(content, file, config) {
-  return sync(postcssProcess(content, file, config))
+  return postcssProcess(content, file, config)
 }
 
 module.exports = exportPlugin(process, info)
